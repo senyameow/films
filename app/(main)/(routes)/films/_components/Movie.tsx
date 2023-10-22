c44a79fdf38522a404e1be191290ed277df488d6
senyameow <arseniyxd@gmail.com>
2023-10-22 23:35:05 +0700

    add video preview

--- app/(main)/(routes)/films/_components/Movie.tsx
+++ app/(main)/(routes)/films/_components/Movie.tsx
@@ -3,6 +3,7 @@ import { Button, buttonVariants } from '@/components/ui/button';
 import { Doc } from '@/convex/_generated/dataModel'
 import { secondsToFilmDuration } from '@/lib/utils';
 import React from 'react'
+import VideoPreview from './VideoPreview';
 
 interface MovieProps {
     film: Doc<'films'>;
@@ -14,7 +15,7 @@ const Movie = ({ film }: MovieProps) => {
         <div key={film._id} className='w-full h-full min-w-[300px] mx-auto relative group bg-zinc-900 col-span'>
             <img src={film.cover_url} alt="film" className='object-cover h-full w-full rounded cursor-pointer transition duration-300 shadow-xl group-hover:opacity-90 sm:group-hover:opacity-0 delay-300' />
             <div className=' opacity-0 group-hover:opacity-100 invisible sm:visible z-10 duration-200 absolute transition top-0 delay-300 group-hover:scale-110 group-hover:-translate-y-[75px] group-hover:translate-x-[20px]'>
-                <img src={film.cover_url} alt="film" className='cursor-pointer object-cover transition duration-300 shadow-xl rounded-t-md w-full ' />
+                <VideoPreview film={film} />
                 <div className='z-10 p-2 lg:p-4 absolute w-full bg-neutral-800 transition shadow-md rounded-b-md'>
                     <div className='flex flex-col gap-4 items-start'>
                         <div className='flex flex-row w-full justify-between gap-2 items-center'>
