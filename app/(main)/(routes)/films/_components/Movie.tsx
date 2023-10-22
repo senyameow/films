9db04ab295aca15aee439933ea8be7b068611265
senyameow <arseniyxd@gmail.com>
2023-10-22 23:22:08 +0700

    add movie view

--- app/(main)/(routes)/films/_components/Movie.tsx
+++ app/(main)/(routes)/films/_components/Movie.tsx
@@ -1,4 +1,7 @@
+import PlayButton from '@/components/PlayButton';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Doc } from '@/convex/_generated/dataModel'
+import { secondsToFilmDuration } from '@/lib/utils';
 import React from 'react'
 
 interface MovieProps {
@@ -12,8 +15,16 @@ const Movie = ({ film }: MovieProps) => {
             <img src={film.cover_url} alt="film" className='object-cover h-full w-full rounded cursor-pointer transition duration-300 shadow-xl group-hover:opacity-90 sm:group-hover:opacity-0 delay-300' />
             <div className=' opacity-0 group-hover:opacity-100 invisible sm:visible z-10 duration-200 absolute transition top-0 delay-300 group-hover:scale-110 group-hover:-translate-y-[75px] group-hover:translate-x-[20px]'>
                 <img src={film.cover_url} alt="film" className='cursor-pointer object-cover transition duration-300 shadow-xl rounded-t-md w-full ' />
-                <div className='z-10 p-2 lg:p-4 absolute w-full bg-neutral-100 transition shadow-md rounded-b-md'>
-                    <span className='text-black'>qwe</span>
+                <div className='z-10 p-2 lg:p-4 absolute w-full bg-neutral-800 transition shadow-md rounded-b-md'>
+                    <div className='flex flex-col gap-4 items-start'>
+                        <div className='flex flex-row w-full justify-between gap-2 items-center'>
+                            <PlayButton id={film._id} className='text-sm' />
+                            <span className='font-semibold'>{secondsToFilmDuration(film.duration)}</span>
+                        </div>
+                        <div className='text-sm font-semibold'>
+                            {film.genre}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -5,12 +5,14 @@ import { Play, PlayCircle } from 'lucide-react'
 import { useAppDispatch } from '@/hooks/redux'
 import { moreInfoSlice } from '@/store/reducers/MoreInfoSlice'
 import { Id } from '@/convex/_generated/dataModel'
+import { cn } from '@/lib/utils'
 
 interface PlayButtonProps {
-    id: Id<'films'>
+    id: Id<'films'>;
+    className?: string;
 }
 
-const PlayButton = ({ id }: PlayButtonProps) => {
+const PlayButton = ({ id, className }: PlayButtonProps) => {
 
     const dispatch = useAppDispatch()
 
@@ -19,9 +21,9 @@ const PlayButton = ({ id }: PlayButtonProps) => {
     }
 
     return (
-        <Button className='hover:opacity-90 transition p-2' onClick={onPlay}>
+        <Button className={cn(`hover:opacity-90 transition p-2`)} onClick={onPlay}>
             <Play className='w-6 h-6 ' />
-            <span className='font-extrabold text-lg'>Play</span>
+            <span className={cn(`font-extrabold text-lg`, className)}>Play</span>
         </Button>
     )
 }
