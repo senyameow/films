45797f7c7b3075582b57096dd8aab6977d25188c
senyameow <arseniyxd@gmail.com>
2023-10-22 23:10:55 +0700

    change toast view

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -6,7 +6,7 @@ import { useUser } from '@clerk/clerk-react'
 import { useMutation, useQuery } from 'convex/react'
 import { Loader2, PlusCircle, X } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 import { toast } from 'sonner'
 
 interface FavoriteButtonProps {
@@ -19,40 +19,48 @@ const FavoriteButton = ({ id, userId }: FavoriteButtonProps) => {
     const addToFavourites = useMutation(api.documents.addFilm)
     const removeFromFavourites = useMutation(api.documents.removeFilm)
 
+    const [isAdding, setIsAdding] = useState(false)
+    const [isRemoving, setIsRemoving] = useState(false)
 
     const user = useQuery(api.documents.getUser, { id: userId })
+    const film = useQuery(api.documents.movie, { id })
+
 
     const isAlreadyIn = user?.favouriteIds?.includes(id)
 
     const onAdd = async () => {
         try {
-            const res = await addToFavourites({ id, userId: userId! })
-            console.log(res)
-            toast.success(`you have successfully added film to your favourites`)
+            setIsAdding(true)
+            await addToFavourites({ id, userId: userId! })
+            toast.success(`you have successfully added ${film?.title} to your favourites`)
         } catch (error) {
             toast.error('something went wrong')
             console.log(error)
+        } finally {
+            setIsAdding(false)
         }
     }
     const onRemove = async () => {
         try {
-            const res = await removeFromFavourites({ id, userId: userId! })
-            console.log(res)
-            toast.success(`you have successfully added removed from your favourites`)
+            setIsRemoving(true)
+            await removeFromFavourites({ id, userId: userId! })
+            toast.success(`you have successfully removed ${film?.title} from your favourites`)
         } catch (error) {
             toast.error('something went wrong')
             console.log(error)
+        } finally {
+            setIsRemoving(false)
         }
     }
 
     return (
         <>
             {isAlreadyIn ? (
-                <button disabled={userId === null} className='hover:opacity-90 transition p-2' onClick={onRemove}>
-                    {userId === null ? <Loader2 className='w-8 h-8 animate-spin' /> : <X className='w-8 h-8 ' />}
+                <button disabled={isRemoving} className='hover:opacity-90 transition p-2' onClick={onRemove}>
+                    {isRemoving ? <Loader2 className='w-8 h-8 animate-spin' /> : <X className='w-8 h-8 ' />}
                 </button>
-            ) : <button disabled={userId === null} className='hover:opacity-90 transition p-2' onClick={onAdd}>
-                {userId === null ? <Loader2 className='w-8 h-8 animate-spin' /> : <PlusCircle className='w-8 h-8 ' />}
+            ) : <button disabled={isAdding} className='hover:opacity-90 transition p-2' onClick={onAdd}>
+                {isAdding ? <Loader2 className='w-8 h-8 animate-spin' /> : <PlusCircle className='w-8 h-8 ' />}
             </button>}
         </>
     )
