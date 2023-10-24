9d301b295d44ac55658af9cd4ede855848cc6139
senyameow <arseniyxd@gmail.com>
2023-10-24 23:26:57 +0700

    add related movies

--- app/(main)/(routes)/films/_components/RelatedMovies.tsx
+++ app/(main)/(routes)/films/_components/RelatedMovies.tsx
@@ -2,6 +2,7 @@
 import { api } from '@/convex/_generated/api';
 import { useQuery } from 'convex/react';
 import React from 'react'
+import Movie from './Movie';
 
 interface RelatedMoviesProps {
     filmGenre: string;
@@ -13,7 +14,12 @@ const RelatedMovies = ({ filmGenre }: RelatedMoviesProps) => {
 
     return (
         <div>
-            {filmGenre}
+            <h2 className='pb-6 text-xl sm:text-2xl xl:text-3xl font-bold'>You may also like</h2>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+                {relatedFilms?.map(film => (
+                    <Movie key={film._id} film={film} />
+                ))}
+            </div>
         </div>
     )
 }
